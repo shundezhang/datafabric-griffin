b5f8dd3098430ba1aeb68099f3f497c5b4671cdc
shundezh <shundezh@bdb69c07-a375-6ffe-1916-a2920873074e>
2010-03-16 02:15:11 +0000

    can use mapfile when connecting to slaves, and allow specifying a default resource

---
+++ griffin/tags/0.6.1/src/main/java/au/org/arcs/griffin/filesystem/FileSystem.java
@@ -0,0 +1,19 @@
+package au.org.arcs.griffin.filesystem;
+
+import java.io.IOException;
+
+
+import org.ietf.jgss.GSSCredential;
+
+import au.org.arcs.griffin.exception.FtpConfigException;
+
+/**
+ * File system interface 
+ * @author Shunde Zhang
+ *
+ */
+public interface FileSystem {
+	public String getPathSeparator();
+	public void init();
+	public FileSystemConnection createFileSystemConnection(GSSCredential credential) throws FtpConfigException, IOException;
+}
