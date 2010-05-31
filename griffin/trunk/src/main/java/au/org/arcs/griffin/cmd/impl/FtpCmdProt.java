bec8970974aa2f9ab0033196a2977bc410d2d766
shundezh <shundezh@bdb69c07-a375-6ffe-1916-a2920873074e>
2010-05-31 00:16:15 +0000

    only support level C for prot command

--- griffin/trunk/src/main/java/au/org/arcs/griffin/cmd/impl/FtpCmdProt.java
+++ griffin/trunk/src/main/java/au/org/arcs/griffin/cmd/impl/FtpCmdProt.java
@@ -76,6 +76,7 @@ import au.org.arcs.griffin.exception.FtpCmdException;
  * </p>
  * 
  * @author Lars Behnke
+ * @author Shunde Zhang
  */
 public class FtpCmdProt extends AbstractFtpCmd {
 
@@ -84,20 +85,27 @@ public class FtpCmdProt extends AbstractFtpCmd {
      */
     public void execute() throws FtpCmdException {
         String level = getArguments().trim().toUpperCase();
-        Boolean ssl = (Boolean) getCtx().getAttribute(ATTR_SSL);
-        if (ssl == null || !ssl.booleanValue()) {
-            msgOut(MSG536);
-        } else if ("C".equals(level)) {
-            getCtx().setAttribute(ATTR_DATA_PROT, Boolean.FALSE);
-            msgOut(MSG200);
-        } else if ("P".equals(level)) {
-            getCtx().setAttribute(ATTR_DATA_PROT, Boolean.TRUE);
-            msgOut(MSG200);
-        } else if ("E".equals(level) || "S".equals(level)) {
-            msgOut(MSG504);
-        } else {
-            msgOut(MSG501);
-        }
+        
+        if (!level.equals("C"))
+            msg("534 Will accept only Clear protection level");
+        else
+        	msgOut(MSG200);
+
+        
+//        Boolean ssl = (Boolean) getCtx().getAttribute(ATTR_SSL);
+//        if (ssl == null || !ssl.booleanValue()) {
+//            msgOut(MSG536);
+//        } else if ("C".equals(level)) {
+//            getCtx().setAttribute(ATTR_DATA_PROT, Boolean.FALSE);
+//            msgOut(MSG200);
+//        } else if ("P".equals(level)) {
+//            getCtx().setAttribute(ATTR_DATA_PROT, Boolean.TRUE);
+//            msgOut(MSG200);
+//        } else if ("E".equals(level) || "S".equals(level)) {
+//            msgOut(MSG504);
+//        } else {
+//            msgOut(MSG501);
+//        }
     }
 
     /**
