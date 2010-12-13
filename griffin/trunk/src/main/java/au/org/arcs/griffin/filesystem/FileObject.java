e51f7aeeb702fa86cd3b4726eba421be0e1dbcbd
Shunde Zhang <shundezhang@users.noreply.github.com>
2010-12-13 04:11:06 +0000

    merged from gridfs branch to fix issues with local fs (cwd, dir listing, etc)

--- griffin/trunk/src/main/java/au/org/arcs/griffin/filesystem/FileObject.java
+++ griffin/trunk/src/main/java/au/org/arcs/griffin/filesystem/FileObject.java
@@ -4,25 +4,58 @@ import java.io.IOException;
 
 /**
  * File object interface
+ * 
  * @author Shunde Zhang
- *
+ * 
  */
 
 public interface FileObject {
-	public String getName();
-	public String getPath();
-	public boolean exists();
-	public boolean isFile();
-	public boolean isDirectory();
-	public int getPermission();
-	public String getCanonicalPath() throws IOException;
-	public FileObject[] listFiles();
-	public long length();
-	public long lastModified();
-	public RandomAccessFileObject getRandomAccessFileObject(String type) throws IOException;
-	public boolean delete();
-	public FileObject getParent();
-	public boolean mkdir();
-	public boolean renameTo(FileObject file);
-	public boolean setLastModified(long t);
+
+    public String getName();
+
+    public String getPath();
+
+    public boolean exists();
+
+    /**
+     * Check for a file.
+     * 
+     * @return True if it is a file.
+     */
+    public boolean isFile();
+
+    /**
+     * Check for a directory.
+     * 
+     * @return True if it is a directory.
+     */
+    public boolean isDirectory();
+
+    /**
+     * Permissions as UNIX file system like integer.
+     * 
+     * @return Permissions.
+     */
+    public int getPermission();
+
+    public String getCanonicalPath() throws IOException;
+
+    public FileObject[] listFiles();
+
+    public long length();
+
+    public long lastModified();
+
+    public RandomAccessFileObject getRandomAccessFileObject(String type)
+            throws IOException;
+
+    public boolean delete();
+
+    public FileObject getParent();
+
+    public boolean mkdir();
+
+    public boolean renameTo(FileObject aFile);
+
+    public boolean setLastModified(long t);
 }
