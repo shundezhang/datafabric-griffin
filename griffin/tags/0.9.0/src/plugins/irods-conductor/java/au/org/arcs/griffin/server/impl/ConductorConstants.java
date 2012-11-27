e2cefc6b7003aea682c9ae4e73beeca8b5f1a232
Shunde Zhang <shundezhang@users.noreply.github.com>
2012-11-27 04:39:51 +0000

    old jargon 3.2.1

--- griffin/tags/0.9.0/src/plugins/irods-conductor/java/au/org/arcs/griffin/server/impl/ConductorConstants.java
+++
@@ -1,6 +0,0 @@
-package au.org.arcs.griffin.server.impl;
-
-public interface ConductorConstants {
-	public static String ATTRIBUTE_REMOTE_GRIDFTP_CONTROL_CHANNELS="remoteGridFTPControlChannels";
-
-}
